75d24246041c6194a209ae6e830c7ee474e5f329
Anass Alilou <alilouanass48@gmail.com>
2026-01-13 16:07:18 +0100

    feat : add shop page and auto-close mobile navbar when clicking outside the toggle menu

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@ const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
 const close = document.getElementById("close");
 
-bar.addEventListener('click', function (){
+bar.addEventListener('click', function (e){
     nav.classList.add('toggle');
-
+    e.stopPropagation();
 });
 close.addEventListener('click', function(){
     nav.classList.remove('toggle');
+});
+
+window.addEventListener('click', function(e) {
+    if (!nav.contains(e.target) && nav.classList.contains('toggle')) {
+        nav.classList.remove('toggle');
+    }
 })
